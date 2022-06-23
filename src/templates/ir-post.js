0bbe2a461d2a789c998af09fe45296ce9f49f632
michinari sasaki <sasaki_michinari@lightcafe.co.jp>
2022-06-23 17:28:10 +0900

    update

--- src/templates/ir-post.js
+++ src/templates/ir-post.js
@@ -2,10 +2,18 @@ import React from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
 
-const IrPost = ({ data }) => {
+const IrPost = ({ data, content, contentComponent, description, tags, title, helmet }) => {
   const { markdownRemark: post } = data;
   return (
     <Layout>
+      <p>プレビュー</p>
+      <p>{content}</p>
+      <p>{contentComponent}</p>
+      <p>{description}</p>
+      <p>{tags}</p>
+      <p>{title}</p>
+      <p>{helmet}</p>
+      <p>プレビューここまで</p>
       <p>{`${post.frontmatter.date}`}</p>
       <p>{`${post.frontmatter.title}`}</p>
       <p>{`${post.frontmatter.description}`}</p>
